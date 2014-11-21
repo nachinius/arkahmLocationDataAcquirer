d36f9494a47f9e403adac5efa7920b21e7c0944f
nachinius <ignacio.peixoto@gmail.com>
2014-11-21 18:00:04 -0300

    Finish crawler, and mythos functions

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,8 +1,6 @@
 /**
- * Utility to retrieve a page, 
- * obtain information about it
- * process that information of it and links
- * and retrieve, process, from those links' content.
+ * Utility to retrieve a page, obtain information about it process that
+ * information of it and links and retrieve, process, from those links' content.
  */
 
 var Q = require('q');
@@ -10,69 +8,145 @@ var REQUEST = require('request');
 var FS = require('fs');
 
 var minimalConfiguration = {
-		baseurl : 'www.arkhamhorrorwiki.com',
-		filenameToSaveData: '',
-		primary: {
-			location: '/Mythos',
-			obtainDataFromHtml: function() {},
+	baseurl : 'www.mybaseaddress.dev',
+	filenameToSaveData : 'callItLikeThis.json',
+	primary : {
+		location : '/SomePart',
+		obtainDataFromHtml : function(html) {
+			return {}
 		},
-		secondary: {
-			obtainLinksFromData: function() {},
-			obtainDataFromHtml: function() {},
-			mergeData: function() {},
+	},
+	secondary : {
+		obtainLinksFromData : function(data) {
+			return [];
 		},
+		obtainDataFromHtml : function(html) {
+			return {};
+		},
+		mergeData : function(primaryData, secondaryData) {
+			return {
+				single : data
+			};
+		},
+	},
 };
 
 /**
- * Merge to an object instance the options and
- * make sure it has the minimalConfiguration as
- * described by `minimalConfiguration` object structure
- *  
+ * Merge to an object instance the options and make sure it has the
+ * minimalConfiguration as described by `minimalConfiguration` object structure
  * 
- * @param instance Object Instance
- * @param minimalConfiguration object
- * @param options object
+ * 
+ * @param instance
+ *            Object Instance
+ * @param minimalConfiguration
+ *            object
+ * @param options
+ *            object
  */
 function mergeOptions(instance, minimalConfiguration, options) {
 	// info must contains all keys and subkeys shown in minimalConfiguration
-	Object.keys(minimalConfiguration).forEach(function(val,idx) {
-		if(typeof options[idx] === 'undefined') {
-			throw ("Crawler Constructor Undefined "+idx);
-		}
-		if(typeof val === 'Object') {
-			Object.keys(val).forEach(function(v2,i2) {
-				if(typeof options[idx][i2] === 'undefined') {
-					throw ("Crawler Constructor: Undefined "+idx+" "+"i2");
+	Object.keys(minimalConfiguration).forEach(
+			function(val, idx) {
+				if (typeof options[idx] === 'undefined') {
+					throw ("Crawler Constructor Undefined " + idx);
+				}
+				if (typeof val === 'Object') {
+					Object.keys(val).forEach(
+							function(v2, i2) {
+								if (typeof options[idx][i2] === 'undefined') {
+									throw ("Crawler Constructor: Undefined "
+											+ idx + " " + "i2");
+								}
+							});
 				}
 			});
-		}
-	});
-	
+
 	// copy options into this object
 	Object.keys(options).forEach(function(val, idx) {
 		instance[idx] = val;
 	});
-	
+
 }
 
 /**
- * @param options Object which must pass the minimalConfiguration
+ * @param options
+ *            Object which must pass the minimalConfiguration
  */
 var Crawler = function(options) {
-	
+
 	/**
 	 * Main file
 	 */
 	this.process = function() {
-		
-		this.requestHtml(this.primary.location)
-		.then(this.primary.obtainDataFromHtml)
-		.then(this.secondary.obtainLinksFromData)
-		.then(this.secondary.obtainDataFromHtml)
-		.then(this.mergeData)
-		.done(this.saveData);
+		var acc = {};
+		this.requestHtml(this.primary.location).then(
+				this.obtainPrimaryDataFromHtml(acc)).then(
+				this.obtainLinksFromPrimaryData(acc)).then(
+				this.transformLinksToAbsolute(acc)).then(
+				this.obtainSecondaryDataFromSecondaryHtml(acc)).then(
+				this.mergePrimaryAndSecondaryData(acc)).then(this.saveData)
+				.done(function() {
+					console.log('FINISHED !');
+				});
 	};
-	
+
+	this.obtainPrimaryDataFromHtml = function(acc) {
+		var that = this;
+		return function(html) {
+			acc.primary = that.primary.obtainDataFromHtml(html);
+			return '';
+		};
+	};
+	this.obtainLinksFromPrimaryData = function(acc) {
+		var that = this;
+		return function() {
+			acc.links = that.secondary.obtainLinksFromData(acc.primary);
+			return '';
+		};
+	};
+	this.transformLinksToAbsolute = function(acc) {
+		var that = this;
+		return function() {
+			acc.links = acc.links.map(function(e, i) {
+				return that.baseurl + '/' + e;
+			});
+			return '';
+		};
+	};
+
+	this.obtainSecondaryDataFromSecondaryHtml = function(acc) {
+		var that = this;
+		return function() {
+			return Q.all(acc.links.map(function(e, i) {
+				console.log('Calling link: ' + e);
+				return that.requestHtml(e).then(function(obj) {
+					console.log('Link obtained: ' + e);
+					var res = that.secondary.obtainDataFromHtml(obj);
+					console.log('Processing link: ' + e);
+					return {
+						e : res
+					};
+				});
+			})).then(function(array) {
+				acc.secondary = array;
+				return '';
+			});
+		};
+	};
+
+	this.mergePrimaryAndSecondaryData = function(acc) {
+		var that = this;
+		return function() {
+			return that.secondary.mergeData(acc.primary, acc.secondary);
+		};
+	};
+
+	this.saveData = function(data) {
+		var that = this;
+		return Q.nfcall(FS.writeFile, that.dataDir + that.filenameToSaveData
+				+ '.json', JSON.stringify(data, null, 1));
+	};
+
 	this.htmlCacheDir = './html/';
 	this.dataDir = './data/';
 
@@ -80,30 +154,32 @@ var Crawler = function(options) {
 	 * Given a url address, a simple identifier.
 	 * 
 	 * In this case, whatever is at the right of '/'
+	 * 
 	 * @return string which does not have any '/'
 	 */
 	this.getLastPart = function(url) {
 		return url.split('/').slice(-1);
-	}
-	
+	};
+
 	/**
 	 * Transform an url address to a filename
+	 * 
 	 * @return string which is a valid filename
 	 */
 	this.urlToCachingFilename = function(url) {
-		return this.htmlCachedDir + this.getLastPart(url) +'.html';
-	}
-	
+		return this.htmlCachedDir + this.getLastPart(url) + '.html';
+	};
+
 	this.requestHttp = function(url, filename) {
 		return Q.nfcall(REQUEST, url).then(function(response) {
-			if(response[0].status === '200') {
+			if (response[0].status === '200') {
 				return response[0].body;
 			} else {
 				return '';
 			}
 		});
-	}
-	
+	};
+
 	/**
 	 * Request a Promise for a Html content of url
 	 * 
@@ -112,24 +188,23 @@ var Crawler = function(options) {
 	this.requestHtml = function(url) {
 		var filename = this.urlToCachingFilename(url);
 		return Q.nfcall(FS.exists, filename).then(function(exists) {
-			if(exists) {
+			if (exists) {
 				return Q.nfcall(FS.readFile, filename);
 			} else {
 				return this.requestHttp(url, filename).then(function(html) {
-					if(html) {
+					if (html) {
 						FS.writeFile(filename, html);
 					}
 					return html;
 				});
 			}
 		});
-	}
-	
-	
-	//mergeOptions(this, minimalConfiguration, options);
-}
+	};
+
+	mergeOptions(this, minimalConfiguration, options);
+};
 
 module.exports = {
-		Crawler: Crawler,
-		minimal: minimalConfiguration
-}
+	Crawler : Crawler,
+	minimal : minimalConfiguration
+};
--- mythos/index.js
+++ mythos/index.js
@@ -3,50 +3,144 @@
  */
 var cheerio = require('cheerio');
 
-function ObtainDataFromMythosHtml(html) {
-    
-    var $ = cheerio.load(html);
-
-    var obj = [];
-
-    var data = $('table tr');
-
-    // take out the header titles
-    var length = data.length;
-    data = data.slice(1, length);
-
-    // gets the content of each row
-    data.map(function(i,e) {
-        obj[i] = {};
-
-        $(e).children('td').map(function(i2,e2) {
-            var ele = $(e2);
-            // first column: mythos name and link
-            switch(i2) {
-                case 0:
-                    obj[i].name = ele.text().replace('\n','').trim();
-                    obj[i].link = ele.children('a').attr('href');
-                    break;
-                case 1:
-                    obj[i].type = ele.text();
-                    break;
-                case 2:
-                    obj[i].expansion = ele.children('a').attr('title');
-                    break;
-                case 3:
-                    obj[i].gate = ele.children('a').attr('title');
-                    break;
-                case 4:
-                    if(ele.children('a').length()>0) {
-                        obj[i].special = ele.text().replace('\n','').trim();
-                    }
-                    break;
-            }
-        });
-
-    }); // data.map
-
-    return obj;
+var clean = function(str) {
+	return str.replace('\n', '').trim();
 }
 
+/**
+ * process the html and extract the information in it
+ * 
+ * @return Array of Object whose keys may be: name, link, type, expansion, gate,
+ *         special
+ */
+var obtainDataFromMythosHtml = function(html) {
 
+	var $ = cheerio.load(html);
+
+	var obj = [];
+
+	var data = $('table tr');
+
+	// take out the header titles
+	var length = data.length;
+	data = data.slice(1, length);
+
+	// gets the content of each row
+	data.map(function(i, e) {
+		obj[i] = {};
+
+		$(e).children('td').map(function(i2, e2) {
+			var ele = $(e2);
+			// first column: mythos name and link
+			switch (i2) {
+			case 0:
+				obj[i].name = clean(ele.text());
+				obj[i].link = ele.children('a').attr('href');
+				break;
+			case 1:
+				obj[i].type = ele.text();
+				break;
+			case 2:
+				obj[i].expansion = ele.children('a').attr('title');
+				break;
+			case 3:
+				obj[i].gate = ele.children('a').attr('title');
+				break;
+			case 4:
+				if (ele.children('a').length() > 0) {
+					obj[i].special = clean(ele.text());
+				}
+				break;
+			}
+		});
+
+	}); // data.map
+
+	return obj;
+}
+
+/**
+ * From the Array of Objects found from main page, obtain the links to follow
+ */
+var obtainLinksFromData = function(obj) {
+	var links = [];
+	obj.forEach(function(v) {
+		if (v.link) {
+			links.push(v.link);
+		}
+	});
+	return links;
+}
+
+/**
+ * Process secondary links
+ * 
+ * @return Object {
+ * 		title: text,
+ * 		type: text,
+ * 		gates: [text],
+ * 		clubs: [text],
+ * 		text: text,
+ * 		movement: {
+ * 			white: text,
+ * 			black: text
+ * 		}
+ * 	}
+ */
+var obtainSecondaryDataFromHtml = function(html) {
+
+	var $ = cheerio.load(html);
+
+	var obj = { title: clean($('h1.firstHeading').text())};
+	
+	var data = $('div.singleLineDL');
+
+
+	data.children('p').each(function(i, e) {
+		switch (i) {
+		case 0:
+			obj.type = clean($(e).text());
+			break;
+		case 1:
+			obj.gates = [];
+			$(e).children('a').map(function(i2, e2) {
+				obj.gates.push($(e2).attr('title'));
+			});
+			break;
+		case 2:
+			obj.clubs = [];
+			$(e).children('a').map(function(i2, e2) {
+				obj.clubs.push($(e2).attr('title'));
+			});
+			break;
+		case 3:
+			obj.text = clean($(e).text());
+			break;
+		}
+	});
+
+	data.find('table span').each(function(i, e) {
+		if (i === 0) {
+			obj.movement = {
+				white : clean($(e).text())
+			};
+		} else if (i === 1) {
+			obj.movement.black = clean($(e).text());
+		}
+	});
+
+	return obj;
+};
+
+var options = {
+	baseurl : 'www.arkhamhorrorwiki.com',
+	filenameToSaveData : 'mythos.json',
+	primary : {
+		location : '/Mythos',
+		obtainDataFromHtml : obtainDataFromMythosHtml
+	},
+	secondary : {
+		obtainLinksFromData : obtainLinksFromData,
+		obtainDataFromHtml : obtainSecondaryDataFromHtml,
+	}
+}
